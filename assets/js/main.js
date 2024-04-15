6eeb1476c287df1f384436a31dc388d976cc4ebf
Jakub Krizka <j.krizka@janderijk.com>
2024-04-15 16:45:46 +0200

    repos info

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,4 +28,20 @@
       ? area.classList.add("blurry")
       : area.classList.remove("blurry");
   });
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let repos = JSON.parse(this.responseText);
+            document.write("<h3>k3llydev github repositories w/last updated</h3>");
+            repos.forEach((repo) => {
+                document.write(`<code>${repo.name}</code>: <em>${new Date(repo.updated_at)}</em><br>`);
+            });
+        }
+    };
+    xhttp.open("GET", "https://api.github.com/users/brokengdnb/repos", true);
+    xhttp.send();
+
+
+
 })();
